3d4e77f64e3f6b4a8ab53590170fc91478bbf0c2
qasimk <qasimworkaus@gmail.com>
2023-02-11 18:35:39 +0000

    Making shift to yolov7 wrapper package
    - yolov7detect in trial

--- source/network.py
+++ source/network.py
@@ -2,3 +2,14 @@
 This file wraps YOLOv7 scripts in abstracted functions.
 The one-stop-shop for all network functionality on this repository.
 '''
+
+import yolov7
+
+def ensure_data_integrity()
+    # Ensuring that dataset is in certain format
+
+def train_model():
+    # Training yolov7
+
+def test_model():
+    # Testing yolov7
