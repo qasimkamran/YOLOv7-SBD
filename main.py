922e0e9fb4f4bc8a45dd5cf464e92c73c26ea9e7
qasimk <qasimworkaus@gmail.com>
2023-02-12 01:32:30 +0000

    Train function first draft
    - Dataset to be configured
    - Directories to be made absolute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from signboard_detector import SignboardDetector
 
 if __name__ == '__main__':
-    detector = SignboardDetector('yolov7/yolov7.pt')
-    detector.train_model()
+    detector = SignboardDetector()
+    detector.train_model("training/yolov7.yaml", "hyp.scratch.custom.yaml")
--- source/signboard_detector.py
+++ source/signboard_detector.py
@@ -3,15 +3,22 @@ This file wraps YOLOv7 scripts in abstracted functions.
 The one-stop-shop for all network functionality on this repository.
 '''
 
+import os
 from yolov7_package import Yolov7Detector
 
 
 class SignboardDetector:
     network = None
 
-    def __init__(self, weights=None):
-        assert weights is not None, f'Must specify model weights'
+    # Yaml directories
+    HYP_DIR = 'hyp'
+    DATA_DIR = 'data'
+    CFG_DIR = 'cfg'
 
+    # Results directories
+    RESULTS_DIR = 'results'
+
+    def __init__(self):
         self.network = Yolov7Detector()
         print('Successfully initialised network!')
         pass
@@ -20,9 +27,18 @@ class SignboardDetector:
         # Ensuring that the dataset is in valid format
         print('Ensure data integrity')
 
-    def train_model(self):
-        # Training yolov7
-        self.network.train()
+    def train_model(self, cfg, hyp):
+        # Stay the same for any configuration
+        data = os.path.join(self.DATA_DIR, 'signboard.yaml')
+        save_dir = os.path.join(self.RESULTS_DIR, 'train')
+
+        cfg = os.path.join(self.CFG_DIR, cfg)
+        hyp = os.path.join(self.HYP_DIR, hyp)
+
+        self.network.train(save_dir=save_dir,
+                           cfg=cfg,
+                           hyp=hyp,
+                           data=data)
 
     def test_model(self):
         # Testing yolov7
