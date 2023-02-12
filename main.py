9af8418e0d04293cade426ddb62d05cd5b710c92
qasimk <qasimworkaus@gmail.com>
2023-02-12 19:21:10 +0000

    Class constructor and show function added
    - Labels read as text string
    - Prediction read as np array

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from signboard_detector import SignboardDetector
+from signboard_recreation import SignboardCreator
 
 if __name__ == '__main__':
-    detector = SignboardDetector()
-    detector.train_model("training/yolov7.yaml", "hyp.scratch.custom.yaml")
+    # detector = SignboardDetector()
+    # detector.train_model("training/yolov7.yaml", "hyp.scratch.custom.yaml")
+
+    creator = SignboardCreator(prediction_path='/home/qasimk/YOLOv7-SBD/yolov7/runs/detect/exp6/blank_input.png',
+                               label_path='/home/qasimk/YOLOv7-SBD/yolov7/runs/detect/exp6/labels/blank_input.txt')
+    creator.show_prediction('Prediction')
--- source/signboard_recreation.py
+++ source/signboard_recreation.py
@@ -1,4 +1,32 @@
 '''
-This file includes all the functionality required to recreated content within
+This file includes all the functionality required to recreate content within
 a signboard prediction through its bounding box crop.
 '''
+
+import cv2
+import os
+import numpy as np
+from PIL import Image
+
+class SignboardCreator:
+
+    labels = None
+    prediction = None
+
+    def __init__(self, label_path, prediction_path):
+        assert os.path.exists(label_path), f'Incorrect file path: {label_path}'
+        assert os.path.exists(prediction_path), f'Incorrect file path: {prediction_path}'
+
+        with open(label_path) as file:
+            self.labels = file.read()
+            print('Read prediction labels as string')
+
+        with Image.open(prediction_path) as image:
+            self.prediction = np.array(image)
+            print('Read prediction image as np array')
+        pass
+
+    def show_prediction(self, win_name):
+        cv2.imshow(win_name, self.prediction)
+        cv2.waitKey(0)
+        cv2.destroyWindow(win_name)
