ca550e4a9cdc95e781c0d728ec25b6bf9dd5f3bd
qasimk <qasimworkaus@gmail.com>
2023-03-04 02:32:10 +0000

    EAST getting super out of hand D:
    - Going to analyse git repo implementation to write my own next
    -Major rework to whatever there is currently, very flawed implementation here

--- source/text_detector.py
+++ source/text_detector.py
@@ -1,14 +1,18 @@
 import keras
 import tensorflow as tf
-from keras import layers
+from keras import layers, models
+from keras import backend as K
 import numpy as np
 import cv2
 import bbox
 
 
 class EAST():
+    model = None
 
-    def __init__(self):
+    def __init__(self, img):
+        self.model = models.Sequential()
+        self.predict(img)
         pass
 
     # Define the image size
@@ -47,55 +51,72 @@ class EAST():
 
     def preprocess(self, img):
         """Preprocess the input image by resizing and normalizing the pixel values."""
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, self.IMAGE_SIZE)
         img = img.astype(np.float32) / 255.0
-        return img()
+        return img
 
     def fcn(self, input):
+        input_tensor = tf.keras.Input(shape=input.shape)
+
         """Define the FCN layers."""
-        conv1 = layers.Conv2D(input, filters=32, kernel_size=(3, 3), padding='same', activation=tf.nn.relu)
-        pool1 = layers.MaxPool2D(conv1, pool_size=(2, 2), strides=(2, 2))
-        conv2 = layers.Conv2D(pool1, filters=64, kernel_size=(3, 3), padding='same', activation=tf.nn.relu)
-        pool2 = layers.MaxPool2D(conv2, pool_size=(2, 2), strides=(2, 2))
-        conv3 = layers.Conv2D(pool2, filters=128, kernel_size=(3, 3), padding='same', activation=tf.nn.relu)
-        pool3 = layers.MaxPool2D(conv3, pool_size=(2, 2), strides=(2, 2))
-        conv4 = layers.Conv2D(pool3, filters=256, kernel_size=(3, 3), padding='same', activation=tf.nn.relu)
-        pool4 = layers.MaxPool2D(conv4, pool_size=(2, 2), strides=(2, 2))
-        conv5 = layers.Conv2D(pool4, filters=512, kernel_size=(3, 3), padding='same', activation=tf.nn.relu)
-        output = layers.Conv2D(conv5, filters=512, kernel_size=(3, 3), padding='same', activation=tf.nn.relu)
+        conv1 = layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu')(input_tensor)
+        pool1 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+        conv2 = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(pool1)
+        pool2 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+        conv3 = layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(pool2)
+        pool3 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv3)
+        conv4 = layers.Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu')(pool3)
+        pool4 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv4)
+        conv5 = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(pool4)
+        output = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(conv5)
         return output
 
-    def rpn(self, input):
+    def rpn(self, input_tensor):
         """Define the RPN layers."""
-        conv1 = layers.Conv2D(input, filters=self.RPN_FILTERS, kernel_size=self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                                 activation=tf.nn.relu)
-        conv2 = layers.Conv2D(conv1, filters=self.RPN_FILTERS, kernel_size=self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                                 activation=tf.nn.relu)
-        conv3 = layers.Conv2D(conv2, filters=self.RPN_FILTERS, kernel_size=self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                                 activation=tf.nn.relu)
-        conv4 = layers.Conv2D(conv3, filters=self.RPN_FILTERS, kernel_size=self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                                 activation=tf.nn.relu)
+        conv1 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
+                              activation='relu')(input_tensor)
+        conv2 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
+                              activation='relu')(conv1)
+        conv3 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
+                              activation='relu')(conv2)
+        conv4 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
+                              activation='relu')(conv3)
 
         # Compute the RPN box scores and reshape the tensor to have 2 channels for each anchor
-        rpn_box_scores = layers.Conv2D(conv4, filters=self.RPN_CHANNELS * self.RPN_BOXES_PER_LOCATION, kernel_size=(1, 1),
-                                          padding=self.BINARY_PADDING)
-        rpn_box_scores = tf.reshape(rpn_box_scores, [-1, tf.shape(rpn_box_scores)[1], tf.shape(rpn_box_scores)[2],
-                                                     self.RPN_BOXES_PER_LOCATION, self.RPN_CHANNELS])
-        rpn_box_scores = tf.transpose(rpn_box_scores, [0, 1, 2, 4, 3])
+        rpn_box_scores = layers.Conv2D(2 * self.RPN_BOXES_PER_LOCATION, (1, 1),
+                                       padding=self.BINARY_PADDING,
+                                       name='rpn_box_scores')(conv4)
+        rpn_box_scores = layers.Reshape((-1, K.int_shape(rpn_box_scores)[2],
+                                         self.RPN_BOXES_PER_LOCATION, 2))(rpn_box_scores)
+        rpn_box_scores = layers.Permute((1, 2, 4, 3))(rpn_box_scores)
 
         # Compute the RPN box coordinates and reshape the tensor to have 4 channels for each anchor
-        rpn_box_coords = layers.Conv2D(conv4, filters=self.RPN_CHANNELS * 4, kernel_size=(1, 1), padding=self.BINARY_PADDING)
-        rpn_box_coords = tf.reshape(rpn_box_coords, [-1, tf.shape(rpn_box_coords)[1], tf.shape(rpn_box_coords)[2],
-                                                     self.RPN_BOXES_PER_LOCATION, 4])
-        rpn_box_coords = tf.transpose(rpn_box_coords, [0, 1, 2, 4, 3])
+        rpn_box_coords = layers.Conv2D(4 * self.RPN_BOXES_PER_LOCATION, (1, 1), padding=self.BINARY_PADDING,
+                                       name='rpn_box_coords')(conv4)
+        rpn_box_coords = layers.Reshape((-1, K.int_shape(rpn_box_coords)[2],
+                                         self.RPN_BOXES_PER_LOCATION, 4))(rpn_box_coords)
+        rpn_box_coords = layers.Permute((1, 2, 4, 3))(rpn_box_coords)
 
         # Compute the anchor boxes and reshape the tensor to have 4 channels for each anchor
-        anchor_boxes = tf.constant(self.generate_anchor_boxes(), dtype=tf.float32)
-        anchor_boxes = tf.reshape(anchor_boxes, [1, 1, 1, self.RPN_NUM_ANCHORS, 4])
-        anchor_boxes = tf.tile(anchor_boxes, [tf.shape(input)[0], tf.shape(input)[1], tf.shape(input)[2], 1, 1])
+        # anchor_boxes = layers.Lambda(lambda x: K.constant(self.generate_anchor_boxes(), dtype=tf.float32))(input_tensor)
+        # anchor_boxes = layers.Reshape((1, 1, self.RPN_NUM_ANCHORS, 4))(anchor_boxes)
+        # anchor_boxes = layers.Lambda(
+        #    lambda x: K.tile(x, (K.shape(input_tensor)[0], K.shape(input_tensor)[1], K.shape(input_tensor)[2], 1, 1)))(anchor_boxes)
+
+        feature_map_height = K.int_shape(input_tensor)[1]
+        feature_map_width = K.int_shape(input_tensor)[2]
+
+        anchor_boxes = self.generate_anchor_boxes()
+        anchor_boxes_tensor = K.variable(anchor_boxes, dtype='float32')
+        tile_multiples = (self.RPN_NUM_ANCHORS, 1, 1)
+        # anchor_boxes = anchor_boxes.reshape(feature_map_height * feature_map_width * self.RPN_NUM_ANCHORS, 4)
+        anchor_boxes_tensor = K.variable(anchor_boxes, dtype='float32')
+        anchor_boxes_tensor = K.reshape(anchor_boxes, (feature_map_height * feature_map_width * self.RPN_NUM_ANCHORS, 4))
+        anchor_boxes_tensor = K.tile(anchor_boxes_tensor, (K.shape(input_tensor)[0], 1, 1, 1, 1))
 
         # Compute the proposal boxes and their scores
-        rpn_proposal_boxes, rpn_proposal_scores = self.proposal_layer(rpn_box_coords, rpn_box_scores, anchor_boxes)
+        rpn_proposal_boxes, rpn_proposal_scores = self.proposal_layer(rpn_box_coords, rpn_box_scores, anchor_boxes_tensor)
 
         return rpn_proposal_boxes, rpn_proposal_scores
 
@@ -106,10 +127,8 @@ class EAST():
             for aspect_ratio in self.RATIOS:
                 width = scale * np.sqrt(aspect_ratio)
                 height = scale / np.sqrt(aspect_ratio)
-                x1 = -width / 2
-                y1 = -height / 2
-                x2 = width / 2
-                y2 = height / 2
+                x1 = y1 = -width / 2
+                x2 = y2 = width / 2
                 anchor_boxes.append([x1, y1, x2, y2])
         return np.array(anchor_boxes)
 
@@ -198,3 +217,25 @@ class EAST():
         clipped_boxes = np.stack([ymin, xmin, ymax, xmax], axis=-1)
 
         return clipped_boxes
+
+    def predict(self, img):
+        # Run inference on a single image.
+        img = self.preprocess(img)
+        fcn_output = self.fcn(img)
+        boxes, scores = self.rpn(fcn_output)
+
+        # Loop over the boxes and scores
+        for box, score in zip(boxes, scores):
+            # Extract the coordinates of the box
+            x1, y1, x2, y2 = box
+
+            # Draw the box on the image
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+            # Add the score to the box
+            cv2.putText(img, str(score), (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+        # Show the image
+        cv2.imshow('Prediction', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
