82fb64a1ca44fb7fcbc4e20593f3561df6558004
qasimk <qasimworkaus@gmail.com>
2023-03-06 15:03:40 +0000

    Working model architecture for EAST
    - Unpool is replicated currently with a bilinear resize
    - ResNet50 instead of PVANet

--- source/text_detector.py
+++ source/text_detector.py
@@ -1,22 +1,28 @@
 import keras
+from keras import layers, models, Input
+from keras.applications.resnet import ResNet50
+import keras.backend as K
 import tensorflow as tf
-from keras import layers, models
-from keras import backend as K
 import numpy as np
 import cv2
-import bbox
+import deeplake
 
 
 class EAST():
     model = None
 
     def __init__(self, img):
-        self.model = models.Sequential()
-        self.predict(img)
+        self.make_model()
+        assert self.model is not None, f'Model unset'
+        score_map, rbox_map = self.predict(img)
+        self.plot_prediction(img, score_map, rbox_map)
         pass
 
-    # Define the image size
-    IMAGE_SIZE = (512, 512)
+    # Define the input size
+    INPUT_SIZE = 512
+
+    # Bilinear resize factor
+    RESIZE_FACTOR = 2
 
     # Define the RPN parameters
     RPN_KERNEL_SIZE = (3, 3)
@@ -52,190 +58,105 @@ class EAST():
     def preprocess(self, img):
         """Preprocess the input image by resizing and normalizing the pixel values."""
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, self.IMAGE_SIZE)
+        img = cv2.resize(img, (self.INPUT_SIZE, self.INPUT_SIZE))
+        img = np.expand_dims(img, axis=0)  # Add batch dimension
         img = img.astype(np.float32) / 255.0
         return img
 
-    def fcn(self, input):
-        input_tensor = tf.keras.Input(shape=input.shape)
+    def resize_bilinear(self, layer):
+        return tf.image.resize(layer, size=[K.int_shape(layer)[1] * self.RESIZE_FACTOR, K.int_shape(layer)[2] * self.RESIZE_FACTOR])
 
-        """Define the FCN layers."""
-        conv1 = layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu')(input_tensor)
-        pool1 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv1)
-        conv2 = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu')(pool1)
-        pool2 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv2)
-        conv3 = layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu')(pool2)
-        pool3 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv3)
-        conv4 = layers.Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu')(pool3)
-        pool4 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(conv4)
-        conv5 = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(pool4)
-        output = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='same', activation='relu')(conv5)
-        return output
+    def make_model(self):
+        input_tensor = Input(shape=(self.INPUT_SIZE, self.INPUT_SIZE, 3), name='input_t')
+        resnet = ResNet50(input_tensor=input_tensor, weights='imagenet', include_top=False, pooling=None)
+        '''
+        Extract feature maps from three different resolutions of tensors
+        Fusion feature maps for EAST formed of 16, 32, and 64 unit shaped tensors
+        Stem feature maps for EAST formed of twice of each fusion map size
+        '''
 
-    def rpn(self, input_tensor):
-        """Define the RPN layers."""
-        conv1 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                              activation='relu')(input_tensor)
-        conv2 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                              activation='relu')(conv1)
-        conv3 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                              activation='relu')(conv2)
-        conv4 = layers.Conv2D(self.RPN_FILTERS, self.RPN_KERNEL_SIZE, padding=self.RPN_PADDING,
-                              activation='relu')(conv3)
+        fusion_tensor_16 = resnet.get_layer('conv5_block3_out').output
+        fusion_tensor_32 = resnet.get_layer('conv4_block6_out').output
+        fusion_tensor_64 = resnet.get_layer('conv3_block4_out').output
+        fusion_tensor_128 = resnet.get_layer('conv2_block2_out').output
 
-        # Compute the RPN box scores and reshape the tensor to have 2 channels for each anchor
-        rpn_box_scores = layers.Conv2D(2 * self.RPN_BOXES_PER_LOCATION, (1, 1),
-                                       padding=self.BINARY_PADDING,
-                                       name='rpn_box_scores')(conv4)
-        rpn_box_scores = layers.Reshape((-1, K.int_shape(rpn_box_scores)[2],
-                                         self.RPN_BOXES_PER_LOCATION, 2))(rpn_box_scores)
-        rpn_box_scores = layers.Permute((1, 2, 4, 3))(rpn_box_scores)
+        # Stage 1
 
-        # Compute the RPN box coordinates and reshape the tensor to have 4 channels for each anchor
-        rpn_box_coords = layers.Conv2D(4 * self.RPN_BOXES_PER_LOCATION, (1, 1), padding=self.BINARY_PADDING,
-                                       name='rpn_box_coords')(conv4)
-        rpn_box_coords = layers.Reshape((-1, K.int_shape(rpn_box_coords)[2],
-                                         self.RPN_BOXES_PER_LOCATION, 4))(rpn_box_coords)
-        rpn_box_coords = layers.Permute((1, 2, 4, 3))(rpn_box_coords)
+        stem_tensor_32 = layers.Lambda(self.resize_bilinear, name='stem_1')(fusion_tensor_16)
+        concat_block_1 = K.concatenate([stem_tensor_32, fusion_tensor_32], axis=3)
 
-        # Compute the anchor boxes and reshape the tensor to have 4 channels for each anchor
-        # anchor_boxes = layers.Lambda(lambda x: K.constant(self.generate_anchor_boxes(), dtype=tf.float32))(input_tensor)
-        # anchor_boxes = layers.Reshape((1, 1, self.RPN_NUM_ANCHORS, 4))(anchor_boxes)
-        # anchor_boxes = layers.Lambda(
-        #    lambda x: K.tile(x, (K.shape(input_tensor)[0], K.shape(input_tensor)[1], K.shape(input_tensor)[2], 1, 1)))(anchor_boxes)
+        conv1x1_block_1 = layers.Conv2D(128, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(1e-5))(concat_block_1)
+        batchnorm1_block_1 = layers.BatchNormalization(momentum=0.997, epsilon=1e-5, scale=True)(conv1x1_block_1)
+        activation1_block_1 = layers.Activation('relu')(batchnorm1_block_1)
 
-        feature_map_height = K.int_shape(input_tensor)[1]
-        feature_map_width = K.int_shape(input_tensor)[2]
+        conv3x3_block1 = layers.Conv2D(128, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(1e-5))(activation1_block_1)
+        batchnorm2_block_1 = layers.BatchNormalization(momentum=0.997, epsilon=1e-5, scale=True)(conv3x3_block1)
+        activation2_block_1 = layers.Activation('relu')(batchnorm2_block_1)
 
-        anchor_boxes = self.generate_anchor_boxes()
-        anchor_boxes_tensor = K.variable(anchor_boxes, dtype='float32')
-        tile_multiples = (self.RPN_NUM_ANCHORS, 1, 1)
-        # anchor_boxes = anchor_boxes.reshape(feature_map_height * feature_map_width * self.RPN_NUM_ANCHORS, 4)
-        anchor_boxes_tensor = K.variable(anchor_boxes, dtype='float32')
-        anchor_boxes_tensor = K.reshape(anchor_boxes, (feature_map_height * feature_map_width * self.RPN_NUM_ANCHORS, 4))
-        anchor_boxes_tensor = K.tile(anchor_boxes_tensor, (K.shape(input_tensor)[0], 1, 1, 1, 1))
+        # Stage 2
 
-        # Compute the proposal boxes and their scores
-        rpn_proposal_boxes, rpn_proposal_scores = self.proposal_layer(rpn_box_coords, rpn_box_scores, anchor_boxes_tensor)
+        stem_tensor_64 = layers.Lambda(self.resize_bilinear, name='stem_2')(activation2_block_1)
+        concat_block_2 = K.concatenate([stem_tensor_64, fusion_tensor_64], axis=3)
 
-        return rpn_proposal_boxes, rpn_proposal_scores
+        conv1x1_block_2 = layers.Conv2D(128, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(1e-5))(concat_block_2)
+        batchnorm1_block_2 = layers.BatchNormalization(momentum=0.997, epsilon=1e-5, scale=True)(conv1x1_block_2)
+        activation1_block_2 = layers.Activation('relu')(batchnorm1_block_2)
 
-    def generate_anchor_boxes(self):
-        """Generate a set of anchor boxes for each pixel in the feature map."""
-        anchor_boxes = []
-        for scale in self.SCALES:
-            for aspect_ratio in self.RATIOS:
-                width = scale * np.sqrt(aspect_ratio)
-                height = scale / np.sqrt(aspect_ratio)
-                x1 = y1 = -width / 2
-                x2 = y2 = width / 2
-                anchor_boxes.append([x1, y1, x2, y2])
-        return np.array(anchor_boxes)
+        conv3x3_block1 = layers.Conv2D(128, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(1e-5))(activation1_block_2)
+        batchnorm2_block_2 = layers.BatchNormalization(momentum=0.997, epsilon=1e-5, scale=True)(conv3x3_block1)
+        activation2_block_2 = layers.Activation('relu')(batchnorm2_block_2)
 
-    def proposal_layer(self, rpn_box_coords, rpn_box_scores, anchor_boxes):
-        """Compute the proposal boxes and their scores."""
-        # Compute the box coordinates and clip them to the image boundaries
-        proposals = self.bbox_transform(anchor_boxes, rpn_box_coords)
-        proposals = self.clip_boxes(proposals, tf.shape(rpn_box_scores)[1:3] * self.FEATURE_STRIDES)
+        # Stage 3
 
-        # Flatten the proposal boxes and their scores
-        proposals = tf.reshape(proposals, [-1, 4])
-        rpn_box_scores = tf.reshape(rpn_box_scores, [-1, self.RPN_BOXES_PER_LOCATION])
+        stem_tensor_128 = layers.Lambda(self.resize_bilinear, name='stem_3')(activation2_block_2)
+        concat_block_3 = K.concatenate([stem_tensor_128, fusion_tensor_128], axis=3)
 
-        # Filter out the proposals with a low score
-        keep_indices = tf.where(tf.greater(rpn_box_scores[:, 1], self.RPN_BOX_SCORE_THRESH))[:, 0]
-        proposals = tf.gather(proposals, keep_indices)
-        rpn_box_scores = tf.gather(rpn_box_scores, keep_indices)
+        conv1x1_block_3 = layers.Conv2D(128, (1, 1), padding='same', kernel_regularizer=keras.regularizers.l2(1e-5))(concat_block_3)
+        batchnorm1_block_3 = layers.BatchNormalization(momentum=0.997, epsilon=1e-5, scale=True)(conv1x1_block_3)
+        activation1_block_3 = layers.Activation('relu')(batchnorm1_block_3)
 
-        # Sort the proposals by their score and select the top N
-        sorted_indices = tf.argsort(rpn_box_scores[:, 1], direction="DESCENDING")
-        top_indices = tf.slice(sorted_indices, [0], [self.RPN_PRE_NMS_TOP_N])
-        top_proposals = tf.gather(proposals, top_indices)
-        top_scores = tf.gather(rpn_box_scores, top_indices)
+        conv3x3_block1 = layers.Conv2D(128, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(1e-5))(activation1_block_3)
+        batchnorm2_block_3 = layers.BatchNormalization(momentum=0.997, epsilon=1e-5, scale=True)(conv3x3_block1)
+        activation2_block_3 = layers.Activation('relu')(batchnorm2_block_3)
 
-        # Apply non-maximum suppression to the top proposals
-        nms_indices = tf.image.non_max_suppression(top_proposals, top_scores[:, 1], max_output_size=self.RPN_POST_NMS_TOP_N,
-                                                   iou_threshold=self.RPN_NMS_THRESH)
-        rpn_proposal_boxes = tf.gather(top_proposals, nms_indices)
-        rpn_proposal_scores = tf.gather(top_scores, nms_indices)
+        # Output
 
-        return rpn_proposal_boxes, rpn_proposal_scores
+        ultimate_conv = layers.Conv2D(32, (3, 3), padding='same', kernel_regularizer=keras.regularizers.l2(1e-5), activation='relu')(activation2_block_3)
 
-    def bbox_transform(self, anchor_boxes, rpn_box_coords):
-        """Transforms anchor boxes to proposal boxes using RPN box coordinates.
+        score_map = layers.Conv2D(1, (1, 1), activation='sigmoid', name='score_map')(ultimate_conv)
 
-        Args:
-            anchor_boxes: numpy array of shape (num_anchors, 4) containing the coordinates
-                of the anchor boxes in the format (x1, y1, x2, y2).
-            rpn_box_coords: numpy array of shape (num_anchors, 4) containing the coordinates
-                of the RPN box coordinates in the format (dx, dy, dw, dh).
+        rbox_geo_map = layers.Conv2D(4, (1, 1), activation='sigmoid', name='rbox_geo_map')(ultimate_conv)
+        rbox_geo_map_relative = layers.Lambda(lambda x: x * self.INPUT_SIZE)(rbox_geo_map)
 
-        Returns:
-            proposals: numpy array of shape (num_anchors, 4) containing the coordinates
-                of the proposal boxes in the format (x1, y1, x2, y2).
-        """
-        x_center = (anchor_boxes[:, 2] + anchor_boxes[:, 0]) / 2
-        y_center = (anchor_boxes[:, 3] + anchor_boxes[:, 1]) / 2
-        width = anchor_boxes[:, 2] - anchor_boxes[:, 0]
-        height = anchor_boxes[:, 3] - anchor_boxes[:, 1]
+        rbox_angle_map = layers.Conv2D(1, (1, 1), activation='sigmoid', name='rbox_angle_map')(ultimate_conv)
+        rbox_angle_map_radians = layers.Lambda(lambda x: (x - 0.5) * np.pi / 2)(rbox_angle_map)
 
-        # Calculate the proposals' coordinates
-        proposals_x_center = x_center + rpn_box_coords[:, 0] * width
-        proposals_y_center = y_center + rpn_box_coords[:, 1] * height
-        proposals_width = width * np.exp(rpn_box_coords[:, 2])
-        proposals_height = height * np.exp(rpn_box_coords[:, 3])
+        rbox_map = K.concatenate([rbox_geo_map_relative, rbox_angle_map_radians], axis=3)
 
-        # Calculate the proposals' coordinates
-        proposals_x1 = proposals_x_center - proposals_width / 2
-        proposals_y1 = proposals_y_center - proposals_height / 2
-        proposals_x2 = proposals_x_center + proposals_width / 2
-        proposals_y2 = proposals_y_center + proposals_height / 2
+        model = models.Model(inputs=input_tensor, outputs=[score_map, rbox_map])
 
-        proposals = np.stack([proposals_x1, proposals_y1, proposals_x2, proposals_y2], axis=1)
-
-        return proposals
-
-    def clip_boxes(self, boxes, image_shape):
-        """
-        Clips boxes to image boundaries.
-        Args:
-            boxes: numpy array of shape [N, 4] containing the coordinates of N boxes
-            image_shape: tuple or list of length 2 or 3, containing the height and width of the image
-        Returns:
-            A numpy array of the same shape as boxes, with the coordinates clipped to the image boundaries
-        """
-        boxes = np.asarray(boxes)
-        image_shape = np.asarray(image_shape)
-
-        # Compute the minimum and maximum values for each dimension
-        ymin = np.maximum(boxes[:, 0], 0)
-        xmin = np.maximum(boxes[:, 1], 0)
-        ymax = np.minimum(boxes[:, 2], image_shape[0])
-        xmax = np.minimum(boxes[:, 3], image_shape[1])
-
-        # Combine the values to form the new boxes
-        clipped_boxes = np.stack([ymin, xmin, ymax, xmax], axis=-1)
-
-        return clipped_boxes
+        self.model = model
 
     def predict(self, img):
         # Run inference on a single image.
         img = self.preprocess(img)
-        fcn_output = self.fcn(img)
-        boxes, scores = self.rpn(fcn_output)
 
-        # Loop over the boxes and scores
-        for box, score in zip(boxes, scores):
-            # Extract the coordinates of the box
-            x1, y1, x2, y2 = box
+        # Make predictions
+        score_map, rbox_map = self.model.predict(img)
 
-            # Draw the box on the image
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        return score_map, rbox_map
 
-            # Add the score to the box
-            cv2.putText(img, str(score), (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    def plot_prediction(self, img, score_map, rbox_map):
+        # Overlay predicted boxes on the original image
+        for i in range(score_map.shape[0]):
+            for j in range(score_map.shape[1]):
+                if np.any(score_map[i, j] > 0.5):  # Only draw boxes with score > 0.5
+                    x, y, w, h, angle = rbox_map[i, j][3]
+                    box = cv2.boxPoints(((x, y), (w, h), angle))
+                    box = np.int0(box)
+                    cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
 
-        # Show the image
-        cv2.imshow('Prediction', img)
+        # Display the image with overlays
+        cv2.imshow('Overlay', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
