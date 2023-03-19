1ce69680a3f3fa85aa7b0104b5d2a47472a4760b
qasimk <qasimworkaus@gmail.com>
2023-03-19 23:24:23 +0000

    Rbox loss fixed, full training successful, testing required

--- source/text_detector.py
+++ source/text_detector.py
@@ -39,8 +39,11 @@ class EAST():
 
         self.model.fit(self.images_data, self.boxes_data, batch_size=1)
 
+        self.model.save('np_data')
 
-        self.plot_prediction(self.predict(img))
+        self.model.save_weights('np_data')
+
+        # self.plot_prediction(self.predict(img))
         pass
 
     # Define the input size
@@ -204,44 +207,50 @@ class EAST():
 
         self.model = model
 
-    def gt_box_to_score(self, box, width, height):
-        converted = np.zeros((box.shape[0], width, height))
-        for i in range(box.shape[0]):
-            for j in range(20):
-                if np.all(box[i, j] == 0):
+    def normalize_coords(self, x1, y1, x2, y2, width, height):
+        x1 = int(round(x1 * width / self.INPUT_SIZE))
+        y1 = int(round(y1 * height / self.INPUT_SIZE))
+        x2 = int(round(x2 * width / self.INPUT_SIZE))
+        y2 = int(round(y2 * height / self.INPUT_SIZE))
+        return x1, y1, x2, y2
+
+    def decrement_out_of_bounds_coords(self, coords, bounds):
+        assert len(coords) == len(bounds), f'Length mismatch is coords and bounds input arrays'
+        decrement = False
+        for i in range(len(coords)):
+            if coords[i] == bounds[i]:
+                decrement = True
+                break
+        for i in range(len(coords)):
+            if coords[i] != 0:
+                coords[i] -= 1
+        return coords
+
+    def gt_boxes_to_score_map(self, boxes, width, height):
+        converted = np.zeros((boxes.shape[0], width, height))
+        for i in range(boxes.shape[0]):
+            for j in range(boxes.shape[1]):
+                if np.all(boxes[i, j] == 0):
                     break
-                x1, y1, x2, y2 = box[i, j]
-                x1 = int(round(x1 * width / self.INPUT_SIZE))
-                y1 = int(round(y1 * height / self.INPUT_SIZE))
-                x2 = int(round(x2 * width / self.INPUT_SIZE))
-                y2 = int(round(y2 * height / self.INPUT_SIZE))
+                x1, y1, x2, y2 = boxes[i, j]
+                x1, y1, x2, y2 = self.normalize_coords(x1, y1, x2, y2, width, height)
                 converted[i, y1:y2, x1:x2] = 1.0
         return converted
 
-    def gt_box_to_rbox(self, box, width, height):
-        # Normalize coordinates of bounding boxes
-        bbox_data_norm = box.copy()
-        bbox_data_norm[:, :, 0] /= self.INPUT_SIZE  # x_min
-        bbox_data_norm[:, :, 1] /= self.INPUT_SIZE  # y_min
-        bbox_data_norm[:, :, 2] /= self.INPUT_SIZE  # x_max
-        bbox_data_norm[:, :, 3] /= self.INPUT_SIZE  # y_max
-
-        # Add extra dimension to match batch size of model output
-        bbox_data_norm = np.expand_dims(bbox_data_norm, axis=0)
-
-        # Create array of zeros with shape (batch_size, 128, 128, 5)
-        rbox_map = np.zeros((box.shape[0]-1, width, height, 5))
-
-        # Insert normalized bounding box data into first four channels
-        rbox_map[:, :, :, 0] = bbox_data_norm[:, :, 0]  # x_min
-        rbox_map[:, :, :, 1] = bbox_data_norm[:, :, 1]  # y_min
-        rbox_map[:, :, :, 2] = bbox_data_norm[:, :, 2]  # x_max
-        rbox_map[:, :, :, 3] = bbox_data_norm[:, :, 3]  # y_max
-
-        # Initialize fifth channel to zero
-        rbox_map[:, :, :, 4] = 0
-
-        return rbox_map
+    def gt_boxes_to_rbox_map(self, boxes, width, height):
+        converted = np.zeros((boxes.shape[0], width, height, 5))
+        for i in range(boxes.shape[0]):
+            for j in range(20):
+                if np.all(boxes[i, j] == 0):
+                    break
+                x1, y1, x2, y2 = boxes[i, j]
+                x1, y1, x2, y2 = self.normalize_coords(x1, y1, x2, y2, width, height)
+                cx, cy, w, h, theta = self.bbox_to_rbox(boxes[i, j])
+                x1, y1, x2, y2 = self.decrement_out_of_bounds_coords([x1, y1, x2, y2],
+                                                                     [128, 128, 128, 128])
+                converted[i, y1, x1] = [cx, cy, w, h, theta]
+                converted[i, y2, x2] = [cx, cy, w, h, theta]
+        return converted
 
     def bbox_to_rbox(self, bbox):
         x1, y1, x2, y2 = bbox
@@ -257,11 +266,11 @@ class EAST():
         y_true_np = y_true.numpy()
         y_pred_np = y_pred.numpy()
 
-        y_true_universal = self.gt_box_to_score(y_true_np, y_pred_np.shape[1], y_pred_np.shape[2])
-        y_true_universal_tensor = tf.convert_to_tensor(y_true_universal)
-        y_true_universal_tensor = tf.cast(y_true_universal_tensor, tf.float32)
+        y_true_score = self.gt_boxes_to_score_map(y_true_np, y_pred_np.shape[1], y_pred_np.shape[2])
+        y_true_score_tensor = tf.convert_to_tensor(y_true_score)
+        y_true_score_tensor = tf.cast(y_true_score_tensor, tf.float32)
 
-        loss = - (y_true_universal_tensor * tf.math.log(y_pred + 1e-10) + (1 - y_true_universal_tensor) * tf.math.log(1 - y_pred + 1e-10))
+        loss = - (y_true_score_tensor * tf.math.log(y_pred + 1e-10) + (1 - y_true_score_tensor) * tf.math.log(1 - y_pred + 1e-10))
         loss = tf.reduce_mean(loss, axis=[0, 1, 2])  # average over batch size and other dimensions
 
         return loss
@@ -270,12 +279,12 @@ class EAST():
         y_true_np = y_true.numpy()
         y_pred_np = y_pred.numpy()
 
-        return  0
+        y_true_rbox = self.gt_boxes_to_rbox_map(y_true_np, y_pred_np.shape[1], y_pred_np.shape[1])
+        y_true_rbox_tensor = tf.convert_to_tensor(y_true_rbox)
+        y_true_rbox_tensor = tf.cast(y_true_rbox_tensor, tf.float32)
 
-        raise Exception
-
-        y_true_angle, y_true_h, y_true_w = tf.split(y_true, [1, 1, 1], axis=-1)
-        y_pred_angle, y_pred_h, y_pred_w = tf.split(y_pred, [1, 1, 1], axis=-1)
+        y_true_cx, y_true_cy, y_true_h, y_true_w, y_true_angle = tf.split(y_true_rbox_tensor, [1, 1, 1, 1, 1], axis=-1)
+        y_true_cx, y_true_cy, y_pred_h, y_pred_w, y_pred_angle = tf.split(y_pred, [1, 1, 1, 1, 1], axis=-1)
 
         # Compute the sin and cos of the angle parameters
         y_true_sin = tf.sin(y_true_angle)
@@ -284,17 +293,17 @@ class EAST():
         y_pred_cos = tf.cos(y_pred_angle)
 
         # Compute the smooth L1 loss for the angle parameter
-        angle_loss = tf.reduce_mean(tf.losses.huber_loss(y_true_sin, y_pred_sin) +
-                                    tf.losses.huber_loss(y_true_cos, y_pred_cos))
+        angle_loss = tf.reduce_mean(tf.compat.v1.losses.huber_loss(y_true_sin, y_pred_sin) +
+                                    tf.compat.v1.losses.huber_loss(y_true_cos, y_pred_cos))
 
         # Compute the smooth L1 loss for the height and width parameters
-        hw_loss = tf.reduce_mean(tf.losses.huber_loss(y_true_h, y_pred_h) +
-                                 tf.losses.huber_loss(y_true_w, y_pred_w))
+        hw_loss = tf.reduce_mean(tf.compat.v1.losses.huber_loss(y_true_h, y_pred_h) +
+                                 tf.compat.v1.losses.huber_loss(y_true_w, y_pred_w))
 
         # Compute the smooth L1 loss for the geometric parameter
         y_true_geo = tf.concat([y_true_sin, y_true_cos, y_true_h, y_true_w], axis=-1)
         y_pred_geo = tf.concat([y_pred_sin, y_pred_cos, y_pred_h, y_pred_w], axis=-1)
-        geo_loss = tf.reduce_mean(tf.losses.huber_loss(y_true_geo, y_pred_geo))
+        geo_loss = tf.reduce_mean(tf.compat.v1.losses.huber_loss(y_true_geo, y_pred_geo))
 
         # Compute the total RBOX map loss
         rbox_map_loss = angle_loss + hw_loss + geo_loss
